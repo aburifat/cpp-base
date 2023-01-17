0847d869d2b90a7d3c712252d772adb5ab4aba54
aburifat <rifat.cse4.bu@gmail.com>
2023-01-17 19:13:06 +0600

    searching

--- algorithm/searching/linear_search.h
+++ algorithm/searching/linear_search.h
@@ -1,8 +1,7 @@
 #ifndef LINEAR_SEARCH_H
 #define LINEAR_SEARCH_H
 
-int linear_search(int *arr, int l, int r, int key)
-{
+int linear_search(int *arr, int l, int r, int key){
     for (int i = l; i <= r; i++){
         if (arr[i] == key)return i;
     }

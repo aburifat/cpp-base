c037171dd36e644968928ffb7707f0ee9b2eed1a
aburifat <rifat.cse4.bu@gmail.com>
2023-01-17 19:12:41 +0600

    sorting

---
+++ algorithm/searching/linear_search.h
@@ -0,0 +1,12 @@
+#ifndef LINEAR_SEARCH_H
+#define LINEAR_SEARCH_H
+
+int linear_search(int *arr, int l, int r, int key)
+{
+    for (int i = l; i <= r; i++){
+        if (arr[i] == key)return i;
+    }
+    return -1;
+}
+
+#endif
